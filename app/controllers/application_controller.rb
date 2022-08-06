daa0e63ea35bd5a1c44e95de8b871a14cb3665a5
EC2 Default User <ec2-user@ip-172-31-11-210.us-east-2.compute.internal>
2022-08-06 05:30:18 +0000

    Hedder_Fotter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :telephone_number, :is_active])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: "public/homes#top"  
+  get '/about' => 'public/homes#about', as:'about'
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+ devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
