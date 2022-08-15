e58ce83e79d97a8dd115512089c34fc8b99e811e
EC2 Default User <ec2-user@ip-172-31-11-210.us-east-2.compute.internal>
2022-08-15 09:08:59 +0000

    Admin Genre

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,8 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+namespace :admin do
+  resources :genres, only: [:index, :create, :edit, :update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
